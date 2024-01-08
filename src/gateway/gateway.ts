7acc7d21bd59fdf79bfe38076d67750a7b0848e3
Tom Tarpey <tomtarpeydev@gmail.com>
2024-01-08 01:51:53 +0000

    add a server socket connection init and a onMessage from the server to make a primitive socket server

--- src/gateway/gateway.ts
+++ src/gateway/gateway.ts
@@ -1,12 +1,24 @@
+import { OnModuleInit } from "@nestjs/common";
 import { MessageBody, SubscribeMessage, WebSocketGateway, WebSocketServer } from "@nestjs/websockets";
 import { Server } from 'socket.io';
 @WebSocketGateway()
-export class DsaGateway {
+export class DsaGateway implements OnModuleInit {
     @WebSocketServer()
     server: Server;
+    
+    onModuleInit() {
+        this.server.on('connection', (socket) => {
+            console.log(socket.id);
+            console.log('connected');
+        })
+    }
 
     @SubscribeMessage('newMessage')
     onNewMessage(@MessageBody() body: any) {
         console.log(body);
+        this.server.emit('onMessage', {
+            msg: "New Message",
+            content: body
+        });
     }
 }
